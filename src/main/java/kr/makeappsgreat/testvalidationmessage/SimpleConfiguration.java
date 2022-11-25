e8b51f630623c4bb8e1769b95dd6103346bdd97f
Gayoun <makeappsgreat@gmail.com>
2022-11-25 17:37:16 +0900

    Add setting of bean name and qualifier

--- src/main/java/kr/makeappsgreat/testvalidationmessage/SimpleConfiguration.java
+++ src/main/java/kr/makeappsgreat/testvalidationmessage/SimpleConfiguration.java
@@ -1,5 +1,6 @@
 package kr.makeappsgreat.testvalidationmessage;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class SimpleConfiguration {
 
-    @Bean
+    @Bean("validationMessageSource")
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages/ValidationMessages");
@@ -19,7 +20,7 @@ public class SimpleConfiguration {
     }
 
     @Bean
-    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+    public LocalValidatorFactoryBean getValidator(@Qualifier("validationMessageSource") MessageSource messageSource) {
         System.out.println(">> 100 " + messageSource.getClass().getName());
         ((ResourceBundleMessageSource) messageSource).getBasenameSet().forEach(s -> System.out.println(">> 110 " + s));
 
