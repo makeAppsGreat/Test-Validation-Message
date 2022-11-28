ee1c4d79fa705c93d72d364aabaa94d75ea9b333
Gayoun <makeappsgreat@gmail.com>
2022-11-28 16:43:11 +0900

    Remove registering messagesource bean

--- src/main/java/kr/makeappsgreat/testvalidationmessage/SimpleConfiguration.java
+++ src/main/java/kr/makeappsgreat/testvalidationmessage/SimpleConfiguration.java
@@ -1,7 +1,5 @@
 package kr.makeappsgreat.testvalidationmessage;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -10,19 +8,11 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class SimpleConfiguration {
 
-    @Bean("validationMessageSource")
-    public MessageSource messageSource() {
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages/ValidationMessages");
         messageSource.setDefaultEncoding("UTF-8");
-
-        return messageSource;
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean getValidator(@Qualifier("validationMessageSource") MessageSource messageSource) {
-        System.out.println(">> 100 " + messageSource.getClass().getName());
-        ((ResourceBundleMessageSource) messageSource).getBasenameSet().forEach(s -> System.out.println(">> 110 " + s));
 
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource);
